d77a2704e08417d3f0e31c5b08d33d05c67cb167
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 15:30:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,8 @@
 def map(source_array, block)
   new_array = []
   i = 0
-  while i < source_array.length do
-    new_array << yield(source_array[i])
+  while i < source_array.length
+    new_array.push(yield(source_array[i]))
     i += 1
   end
   new_array
