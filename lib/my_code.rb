6e3748c43aba986b5189ceaacaa02cf7f917bd33
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 15:38:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,5 +10,16 @@ def map(source_array)
 end
 
 def reduce(source_array, starting_value=nil)
-
+  if starting_value
+    new_value = starting_value
+  else
+    new_value = 0
+  end
+  
+  i = 0
+  while i < source_array.length
+    yield(new_value, source_array[i])
+    i += 1
+  end
+  new_value
 end
