0a74f5640a67e6920f869cbeb8563d88b7db35be
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 15:40:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,11 +12,12 @@ end
 def reduce(source_array, starting_value=nil)
   if starting_value
     new_value = starting_value
+    i = 0
   else
-    new_value = 0
+    new_value = source_array[0]
+    i = 1
   end
 
-  i = 0
   while i < source_array.length
     new_value = yield(new_value, source_array[i])
     i += 1
